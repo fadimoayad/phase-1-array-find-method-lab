b75ba3eadf54fb8d428ecc14bb3c45f7d63aa52e
fadimoayad <fadimoayad99@yahoo.com>
2023-01-13 16:17:31 +0300

    Completet Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record){
+const result =record.find(record => record.result==="W")
+if(result){
+return result.year;
+}
+else {
+    return undefined
+    
+}
+}
